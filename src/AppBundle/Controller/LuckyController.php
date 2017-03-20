b9cee06868f135b38c6e838f20d504e69a78fb58
ldevernay <boblehamster@yahoo.fr>
2017-03-20 15:52:18 +0100

    first page

---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,23 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+}
+?>
